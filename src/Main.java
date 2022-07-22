06c781f08ca224264450c8c60b8b7c3bde12f4ab
popponkk <771282250@qq.com>
2022-07-22 13:36:10 +0800

    测试数组排序

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Integer[] ns = { 1, 4, 9, 16, 25 };
+        List<Integer> ls= Arrays.asList(ns);
+        ls=new ArrayList<>(ls);
+        Collections.reverse(ls);
+        System.out.println(ls);
+        System.out.println(Arrays.toString(ns));
     }
 }
